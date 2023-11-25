c2b3e7307adbe81fc1a6c604ce9df56152576d71
DavidOliveirax <davidoliveirax92@gmail.com>
2023-11-25 02:21:11 -0300

    Atualizações 25/11 : 02:20

--- script.js
+++ script.js
@@ -7,18 +7,3 @@ function clickMenu() {
         itens.style.display = 'flex'
     }
 }
-
-/* Carrossel */
-
-const imgs = document.getElementById("img")
-const img = document.querySelectorAll("#img img")
-let idx = 0
-
-function carrossel(){
-    idx++
-    if(idx > img.length - 1){
-        idx = 0
-    }
-    img.style.transform = `translateX(${-idx * 500}px)`
-}
-setInterval(carrossel, 1800)
