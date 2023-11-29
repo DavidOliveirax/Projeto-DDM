56f00ff7c770305cb717bb562a6b05adff0dbf35
DavidOliveirax <davidoliveirax92@gmail.com>
2023-11-28 21:52:35 -0300

    Atualizações 28/11 - 21:52

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 /* Function Burguer */
 
 function clickMenu() {
-    if (itens.style.display  == 'flex') {
+    if (itens.style.display == 'flex') {
         itens.style.display = 'none'
-    }  else {
+    } else {
         itens.style.display = 'flex'
     }
+}
+
+/* Function Slide */
+let slideIndex = 0
+showSlides()
+
+function showSlides() {
+    let i
+    let slides = document.getElementsByClassName("mySlide")
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slideIndex++
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+
+    slides[slideIndex - 1].style.display = "flex"
+    setTimeout(showSlides, 3000)
 }
